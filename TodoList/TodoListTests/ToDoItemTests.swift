8dd8afee972c611f83329c48fe13565ee982425b
Abhishek Sheth <abhishek.sheth@hotmail.com>
2016-08-13 12:22:00 +0530

    Added ToDoItem Test Class

---
+++ TodoList/TodoListTests/ToDoItemTests.swift
@@ -0,0 +1,25 @@
+//
+//  ToDoItemTests.swift
+//  TodoList
+//
+//  Created by Abhishek Sheth on 13/08/16.
+//  Copyright © 2016 Abhishek. All rights reserved.
+//
+
+import XCTest
+
+class ToDoItemTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    
+    
+}
