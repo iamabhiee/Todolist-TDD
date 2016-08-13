40dca75ea076a336d10f8f8023d850b40c843e44
Abhishek Sheth <abhishek.sheth@hotmail.com>
2016-08-13 12:04:58 +0530

    Project Setup

---
+++ TodoList/TodoList/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TodoList
+//
+//  Created by Abhishek Sheth on 13/08/16.
+//  Copyright © 2016 Abhishek. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
